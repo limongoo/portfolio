ba6278e86a0fedaa6e811b0a16f7cfef68e2431f
ivan limongan <ivan.limongan@gmail.com>
2017-10-19 09:57:54 -0700

    media query css work for mobile css

--- js/app.js
+++ js/app.js
@@ -28,19 +28,25 @@ function makeItems() {
     createItem.setAttribute("id", "projectItem");
 
     var createDiv = document.createElement("div");
-    createDiv.setAttribute("id", "projectBox");
+    createDiv.setAttribute("id", "projectBox");    
 
     var createImg = document.createElement("img");
     createImg.setAttribute("id", "imgShow");
     createImg.src = "img/" + pro.image;
 
-    var createTitle = document.createElement('h2');
-    createTitle.setAttribute("id", "projectName");
-    createTitle.innerText = pro.title;
+    // var createTitle = document.createElement('h2');
+    // createTitle.setAttribute("id", "projectName");
+    // createTitle.innerText = pro.title;
+
+    var createLink = document.createElement("a");
+    createLink.href = pro.link;
+    // createLink = document.createElement('h2');
+    createLink.setAttribute("id", "projectName");
+    createLink.innerText = pro.title;
 
     createDiv.appendChild(createImg);
     createItem.appendChild(createDiv);
-    createItem.appendChild(createTitle);
+    createItem.appendChild(createLink);
     get.appendChild(createItem);
     }
 }
