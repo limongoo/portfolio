c0e9e7c51b97f50df32848bf31f532c03609ccde
ivan limongan <ivan.limongan@gmail.com>
2017-12-22 13:50:13 -0800

    added reveal js

--- public/vendors/scripts/scroll.js
+++ public/vendors/scripts/scroll.js
@@ -1,21 +1,33 @@
 // scroll reveal - Scroll Reveal from https://scrollrevealjs.org/
-function addImgClass () {
-//   var pi = document.getElementById('projectContainer');
-  var pimg = document.getElementById('projectItem');
-  var i = 0;  
-  for (i=0; i <pimg.length; i++) {
-      pimg[i].setAttribute('class', 'sr');
-    }
-}
-addImgClass();
+// function addImgClass () {
+// //   var pi = document.getElementById('projectContainer');
+//   var pimg = document.getElementById('projectItem');
+//   var i = 0;  
+//   for (i=0; i <pimg.length; i++) {
+//       pimg[i].setAttribute('class', 'sr');
+//     }
+// }
+// addImgClass();
+
+// window.sr = ScrollReveal({
+//   origin: 'bottom',
+//   duration: 2000,
+//   delay: 1,
+//   opacity: 0,
+//   useDelay: 'always',
+//   reset: false,
+// });
+// var revealContainer = document.getElementById('projectContainer');
+// sr.reveal('.sr', {container: revealContainer});
+
 
 window.sr = ScrollReveal({
+  reset: true,
   origin: 'bottom',
-  duration: 2000,
-  delay: 1,
   opacity: 0,
-  useDelay: 'always',
-  reset: false,
+  duration: 2000,
+  delay: 0.8,
+  scale: 1
 });
-var revealContainer = document.getElementById('projectContainer');
-sr.reveal('.sr', {container: revealContainer});
+sr.reveal('.scroll');
+// sr.reveal('.scrolldiv', {container: '#container'});
